4bd1c2dd2e6d5e72abffee67b9ab0a267f172d89
Nicholas Cotton <nic@nintu.net>
2024-03-19 16:08:27 -0400

    Added .env

--- server.js
+++ server.js
@@ -12,3 +12,8 @@ app.use("/videos", videos);
 app.listen(PORT, () => {
   console.log(`BrainFlix-backend Listening on port: ${PORT}`);
 });
+
+// ! For BandSite Sprint 3, add a .env file to each the client and server folders.
+// *For the client, you just need to add the ‘API url’ and use it.
+// *For the server add the PORT and use it.  You’ll need to install the dotenv package (npm install dotenv).
+// !Remember to add .env to the gitignore files.  Include a .env.example and ask if something doesn’t make sense.
